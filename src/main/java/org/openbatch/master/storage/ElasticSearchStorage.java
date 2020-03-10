77e6282671ce7250fa59e22d8b5611431309361f
jake <jacopo.r00ta@gmail.com>
2020-03-10 22:29:26 +0100

    fix tests

--- src/main/java/org/openbatch/master/storage/ElasticSearchStorage.java
+++ src/main/java/org/openbatch/master/storage/ElasticSearchStorage.java
@@ -1,6 +1,5 @@
 package org.openbatch.master.storage;
 
 public class ElasticSearchStorage implements IStorage {
-
     // TODO
 }
--- src/main/java/org/openbatch/master/storage/InMemoryStorage.java
+++ src/main/java/org/openbatch/master/storage/InMemoryStorage.java
@@ -1,5 +1,8 @@
 package org.openbatch.master.storage;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class InMemoryStorage implements IStorage {
 
 }
